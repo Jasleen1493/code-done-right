64cdb788847d23ff70210426ba1e0c50cd0c7e6d
Jasleen <14.jasleen.93@gmail.com>
2020-03-29 23:26:13 +0530

    Springboot application initial setup

---
+++ processor/src/test/java/com/credit/card/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.card.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
